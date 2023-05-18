70c5aabbce88393e162d561901b069c2525bde21
Regis066 <mugisharegis72@gmail.com>
2023-05-18 11:10:49 +0200

    added some styles

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { TodoWrapper } from './components/TodoWrapper';
 
 function App() {
+  //When you have completed a task click on it then it will be completed.
   return (
     <div className="App">
       <TodoWrapper  />
